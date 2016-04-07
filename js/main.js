cd46abf218ae9ee246d164af821f07c575d83e67
TormundsMember <thisistotallylegit@outlook.com>
2016-04-07 18:37:35 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,14 +5,9 @@ function nextCompetitor(){
 function newCompetitor(){
   $('#collapseExample').collapse('show');
   $('#list').collapse('hide');
-  
-  $('#compButton').hide();
 }
 
 function hideCompetitorWindow(){
-  
-  
   $('#collapseExample').collapse('hide');
   $('#list').collapse('show');
-  $('#compButton').show();
 }
