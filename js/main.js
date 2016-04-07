119eac2efe9edcece113de1f4926835c439bf093
TormundsMember <thisistotallylegit@outlook.com>
2016-04-07 18:23:19 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,5 +3,11 @@ function nextCompetitor(){
 }
 
 function newCompetitor(){
+  $('#collapseExample').collapse('show');
+  $('#list').collapse('hide');
+}
 
+function hideCompetitorWindow(){
+  $('#collapseExample').collapse('hide');
+  $('#list').collapse('show');
 }
