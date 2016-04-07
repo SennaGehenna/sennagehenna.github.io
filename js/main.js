db1ae8550785d5f3d117dd94932bdc4ca4a96991
TormundsMember <thisistotallylegit@outlook.com>
2016-04-07 19:04:15 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+var people = [];
+var regexp = new RegExp("^\\/d20(\\+|\\-)\\d+$")
+var regexp2 = new RegExp("^\\-?\\\d+$");
+
+var _count = 0;
+
 function nextCompetitor(){
 
 }
@@ -10,4 +16,32 @@ function newCompetitor(){
 function hideCompetitorWindow(){
   $('#collapseExample').collapse('hide');
   $('#list').collapse('show');
+}
+
+
+function addCompetitor(){
+  var valid = true;
+  var person = {};
+  var init = $('#Initiative').val();
+  var mod = -1;
+  if(regexp.test(init)){
+    mod = parseInt(init.substr(4)) + Math.floor((Math.random() * 20) + 1);
+  }
+  else if (regexp2.test(init)){
+    mod = parseInt(init);
+  }else{
+    $('#Initiative').attr("class", $('#Initiative').attr("class") + " error");
+    valid = false;
+  }
+  if(regexp2.test($('#AC').val())){
+    person.id = _count++;
+    person.initiative = mod;
+    person.name = $('#Name').val();
+    hideCompetitorWindow();
+  }
+  else{
+    valid = false;
+    $('#AC').attr("class", $('#AC').attr("class") + " error");
+  }
+  }
 }
