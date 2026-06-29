0b4c76ff258ac92c473614b93965c22e28f29291
Anna Fall <thisistotallylegit@outlook.com>
2026-06-29 11:47:28 +0200

    Update code.js to include price per day

--- pages/gas_calc/code.js
+++ pages/gas_calc/code.js
@@ -20,7 +20,8 @@ function calculate(){
   km_one_way = document.getElementById("km_one_way")
   gas_price = document.getElementById("gas_price")
   gas_used = document.getElementById("gas_used")
-  gas_paid = document.getElementById("gas_paid")
+  gas_paid_day = document.getElementById("gas_paid_day")
+  gas_paid_week = document.getElementById("gas_paid_week")
   gas_paid_month = document.getElementById("gas_paid_month")
 
   if(km_one_way.value == undefined || gas_price.value == undefined || gas_used.value == undefined)
@@ -36,10 +37,12 @@ function calculate(){
 
   if(isNaN(km_one_way_value) || isNaN(gas_price_value) || isNaN(gas_used_value))
     return;
-  per_week = (km_one_way_value * 2 * 5 * gas_price_value * gas_used_value / 100).toFixed(2);
+  per_day = (km_one_way_value * 2 * gas_price_value * gas_used_value / 100).toFixed(2);
+  per_week = (per_day * 5).toFixed(2);
+  per_month = (per_week * 4).toFixed(2);
   
-  gas_paid.textContent = "€"+per_week;
-
-  gas_paid_month.textContent = "€" + (per_week*4).toFixed(2);
+  gas_paid_day.textContent = "€"+per_day;
+  gas_paid_week.textContent = "€"+per_week;
+  gas_paid_month.textContent = "€" + per_month;
 
 }
